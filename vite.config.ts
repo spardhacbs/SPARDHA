07f52cb399634712cd90868ef185f1c7a3c9a4c7
spardhacbs <spardhacbs@gmail.com>
2026-09-14 08:44:34 +0530

    Update Vite base path to /SPARDHA_PHASE_II/

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { fileURLToPath, URL } from 'node:url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/SPARDHA/',
+  base: '/SPARDHA_PHASE_II/',
   plugins: [react()],
   resolve: {
     alias: {
