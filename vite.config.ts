8322efa2652d20dc1ce7471b482ce8dc7820c84e
spardhacbs <spardhacbs@gmail.com>
2026-09-18 09:46:32 +0530

    Revert Vite base path to /SPARDHA_PHASE_II/

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { fileURLToPath, URL } from 'node:url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/SPARDHA_BACKUP/',
+  base: '/SPARDHA_PHASE_II/',
   plugins: [react()],
   resolve: {
     alias: {
